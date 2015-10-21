10312b3651d8a2fa91ae5b503456499da8f45f6e
sola <sola931@gmail.com>
2015-10-22 00:43:56 +0200

    Main panel full width, with intelligent mode feature - stage 1

---
+++ general/sh/main_panel_mode.sh
@@ -0,0 +1,2 @@
+xfconf-query -c xfwm4 -p /general/margin_bottom -s $1
+xfconf-query -c xfce4-panel -p /panels/panel-1/autohide-behavior -s $2
--- general/sh/show.sh
+++ general/sh/show.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-mode=1
-bottom=0
+mode=0
 
-if [ $1 -ne 1 ]; then
-
-	if [ "$2" == true ]; then bottom=50;
-	elif [ "$3" == true ]; then bottom=102; fi
-	
-	xfconf-query -c xfwm4 -p /general/margin_bottom -s $bottom
-	mode=0
+if [ $1 -eq 1 ]; then mode=1
+elif [ $1 -eq 2 ]; then bottom=102
+else bottom=50
 fi
 
+xfconf-query -c xfwm4 -p /general/margin_bottom -s $bottom
 xfconf-query -c xfce4-panel -p /panels/panel-$1/autohide-behavior -s 0
 xfconf-query -c xfce4-panel -p /panels/panel-$1/autohide-behavior -s $mode
 xfconf-query -c xfce4-panel -p /panels/panel-$1/length-adjust -s true
+xfconf-query -c xfce4-panel -p /panels/panel-$1/length -s 100
 xfconf-query -c xfce4-panel -p /panels/panel-$1/enter-opacity -s 100
 xfconf-query -c xfce4-panel -p /panels/panel-$1/leave-opacity -s 100
