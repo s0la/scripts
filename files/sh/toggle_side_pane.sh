59fc167729d9f570a151d88e36bd4975c116063f
s0la <sola931@gmail.com>
2016-04-24 23:27:25 +0200

    improved toggle side pane sh to work with active windows

--- files/sh/toggle_side_pane.sh
+++ files/sh/toggle_side_pane.sh
@@ -8,16 +8,23 @@ function toggle_side_pane {
 }
 
 if [ "$files" ]; then
-	for file_id in ${files[*]}; do
+	active_window_id=$(printf 0x%x $(xdotool getactivewindow))
 
-		if [ -z "$(xwininfo -all -id $file_id | grep 'Hidden')" ]; then
+	for file_id in ${files[*]}; do
+		if [ ${active_window_id:(-6)} == ${file_id:(-6)} ]; then
 			wmctrl -i -a $file_id
-			break
+			toggle_side_pane
+			exit
 		fi
 
-		wmctrl -i -a ${files[0]}
+		if [ -z "$(xwininfo -all -id $file_id | grep 'Hidden')" ]; then
+			visible=$file_id
+		else
+			hidden=$file_id
+		fi
 	done
 
+	wmctrl -i -a $([ "$visible" ] && echo $visible || echo $hidden)
 	toggle_side_pane
 else
 	thunar
